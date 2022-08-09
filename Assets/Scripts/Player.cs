6a7406f7b3cf2d99e4907003fe58d15adcf1792a
lizbethnv4 <79164828+lizbethnv4@users.noreply.github.com>
2022-08-09 15:40:41 -0400

    Ultimos cambios.
    Se coloco que se pueda mover de izquierda a derecha el jugador.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private void Update()
+    {
+       // Vector3 position = transform.position;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.position += Vector3.left * this.speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position += Vector3.right * this.speed * Time.deltaTime;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+        }
+    }
+
+
+}
